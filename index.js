a13d481f52e6bbe7dcb6b360526eee52075365da
Pratik <pratik080.rejoice@gmail.com>
2026-01-21 15:37:35 +0530

    add: javascript variables

---
+++ index.js
@@ -0,0 +1,17 @@
+console.log("Imported script");
+
+// variables
+let girl1 = "Vedika";
+console.log("girl1", girl1);
+girl1 = "Pratika";
+console.log("girl1", girl1);
+// let - can be reassigned, but can not be redeclared
+
+var girl2 = "Vanshika";
+girl2 = "Nandini";
+// var girl2 = "Juhi";
+console.log("girl", girl2);
+// var - can be reassigned as well as redeclared
+
+const girl3 = "Nidhi";
+console.log("girl3", girl3);
