4ab3e1a0950fbdeb075c35c3ee3cf3d30481eb6e
Pratik <pratik080.rejoice@gmail.com>
2026-02-18 11:22:42 +0530

    add: dom manipulation

--- domManipulation.js
+++ domManipulation.js
@@ -1,23 +1,38 @@
-console.log(document);
+// console.log(document);
 
-// getElementById
-const vedikaEl = document.getElementById("h1Element");
-vedikaEl.style.backgroundColor = "skyBlue";
-vedikaEl.innerText = "Text is changed now";
-console.log(vedikaEl);
+// // getElementById
+// const vedikaEl = document.getElementById("h1Element");
+// vedikaEl.style.backgroundColor = "skyBlue";
+// vedikaEl.innerText = "Text is changed now";
+// console.log(vedikaEl);
 
-// getElementsByClassName
-const pElement = document.getElementsByClassName("p1");
-for (let i = 0; i < pElement.length; i++) {
-  const element = pElement[i];
-  element.style.color = "pink";
-}
-console.log(pElement);
+// // getElementsByClassName
+// const pElement = document.getElementsByClassName("p1");
+// for (let i = 0; i < pElement.length; i++) {
+//   const element = pElement[i];
+//   element.style.color = "pink";
+// }
+// console.log(pElement);
 
-// getElementsByTagName
-const pElements = document.getElementsByTagName("p");
-for (let i = 0; i < pElements.length; i++) {
-  const element = pElements[i];
-  element.style.color = "green";
-}
-console.log(pElements);
+// // getElementsByTagName
+// const pElements = document.getElementsByTagName("p");
+// for (let i = 0; i < pElements.length; i++) {
+//   const element = pElements[i];
+//   element.style.color = "green";
+// }
+// console.log(pElements);
+
+// const el = document.querySelector("#h1Element");
+// const el = document.querySelector(".p1");
+// const el = document.querySelectorAll("p");
+// console.log(el);
+
+// document.addEventListener("scroll", () => {
+//   console.log("testing event listner");
+// });
+
+// const newDiv = document.createElement("div");
+// newDiv.style.background = "coral";
+// newDiv.appendChild(el);
+
+// console.log(bodyEl);
